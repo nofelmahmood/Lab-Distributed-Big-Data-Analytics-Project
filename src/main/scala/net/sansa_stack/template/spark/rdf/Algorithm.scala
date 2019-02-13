a854bf3a90a2016f4867aaceabe781f1a1564df2
nofelmahmood <nofelmehmood@gmail.com>
2019-02-13 23:07:32 +0100

    New methods for gamma and blocking scheme

--- src/main/scala/net/sansa_stack/template/spark/rdf/Algorithm.scala
+++ src/main/scala/net/sansa_stack/template/spark/rdf/Algorithm.scala
@@ -1,16 +1,19 @@
 package net.sansa_stack.template.spark.rdf
 
 import scala.Predef._
-
 import org.apache.spark.SparkContext._
-
-import org.apache.spark.sql.SQLImplicits
-
-import org.apache.spark.sql.{DataFrame, SparkSession, types}
+import org.apache.spark.sql._
 import com.rockymadden.stringmetric.phonetic.RefinedSoundexMetric
 import org.apache.spark.sql.types._
+import org.datasyslab.geosparksql.utils.DataFrameFactory
 
 object Algorithm {
+
+  case class FeatureVector(Tuple: String,
+                           Author: Integer,
+                           Title: Integer,
+                           Venue: Integer,
+                           Review: Integer)
 
   def main(args: Array[String]): Unit = {
     run("dataset.csv")
@@ -29,33 +32,38 @@ object Algorithm {
       .appName(s"Spark CSV Reader")
       .getOrCreate()
 
-
     val resourcePath = "src/main/resources/dataset.csv"
     val df = spark.read.format("csv").option("header", "true").load(resourcePath)
 
     val featureVectors = generateFeatureVectors(spark, df)
-    val sampleSize = 10
+
+    val sampleSize = 20
     val sample = randomSample(featureVectors, sampleSize)
-    sample.show()
-    println(s"Sample Count ${sample.count()}")
+
     val authorsGamma = calculateGamma(sample, "Author")
     val titlesGamma = calculateGamma(sample, columnName = "Title")
     val venueGamma = calculateGamma(sample, columnName = "Venue")
     val reviewGamma = calculateGamma(sample, columnName = "Review")
 
-    println(s"Author Gamma ${authorsGamma}")
-    println(s"Titles Gamma ${titlesGamma}")
-    println(s"Venues Gamma ${venueGamma}")
-    println(s"Reviews Gamma ${reviewGamma}")
+    val authorsDeficient = authorsGamma * sample.count()
+    val titlesDeficient = titlesGamma * sample.count()
+    val venueDeficient = venueGamma * sample.count()
+    val reviewDeficient = reviewGamma * sample.count()
+
+    val featureVectorsExceptSample = featureVectors.exceptAll(sample)
+    println(s"Feature Vectors count ${featureVectors.count()}")
+    println(s"Sample Count ${sample.count()}")
+    println(s"Feature Vectors Except Sample ${featureVectorsExceptSample.count()}")
+
+    // Add similar or dissimilar samples
+    addSample(sample, featureVectorsExceptSample,
+      "Author",
+      authorsDeficient.toInt,
+      authorsDeficient <= 0)
+
 
     spark.stop
   }
-
-  case class FeatureVector(Tuple: String,
-                           Author: Integer,
-                           Title: Integer,
-                           Venue: Integer,
-                           Review: Integer)
 
   def generateFeatureVectors(spark: SparkSession, dataframe: DataFrame): DataFrame = {
 
@@ -125,11 +133,10 @@ object Algorithm {
   }
 
   def randomSample(dataFrame: DataFrame, size: Int): DataFrame = {
-    val sample = dataFrame.sample(false, 0.1, size)
-
+    val sample = dataFrame.sample(false, 1D*size/dataFrame.count)
     return sample
   }
-
+  
   def calculateGamma(sample: DataFrame, columnName: String): Float = {
     val column = sample.select(columnName)
     val count = column.count().toInt
@@ -149,6 +156,58 @@ object Algorithm {
     return gamma
   }
 
+  def satisfiesBlockingScheme(featureVector: Row,
+                              columnIndexes: Array[Int],
+                              conjunctionsAndDisjunctions: Array[Boolean]): Boolean = {
+
+    val firstColumnIndex = columnIndexes(0)
+    var result = featureVector.getInt(firstColumnIndex) == 1
+
+    for (index <- 0 to (conjunctionsAndDisjunctions.length - 1)) {
+
+      val columnIndex = columnIndexes(index + 1)
+      val featureValue = featureVector.getInt(columnIndex) == 1
+      val condition = conjunctionsAndDisjunctions(index)
+
+      // Condition == true means and
+      // Condition == false means or
+
+      if (condition) {
+        result = result && featureValue
+      } else {
+        result = result || featureValue
+      }
+    }
+
+    return result
+  }
+
+  def addSample(sampleDataframe: DataFrame,
+                dataframe: DataFrame,
+                columnName: String,
+                deficiency: Int,
+                similar: Boolean): Unit = {
+
+    val column = dataframe.select(columnName)
+
+
+
+    if (similar) {
+      var selectedRows: Vector[FeatureVector] = Vector()
+
+      dataframe.foreach { row =>
+        val author = row.getInt(0)
+        println(s"Author ${author}")
+
+      }
+
+    } else {
+
+      for (i <- 0 to deficiency) {
+
+      }
+    }
+  }
 
 
 //  case class Config(in: String = "")
